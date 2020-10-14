38cc52f0066fbf07c13000b9605382f1ccdb0696
njmihakim <72840942+njmihakim@users.noreply.github.com>
2020-10-14 12:35:01 +0800

    Add files via upload

---
+++ Shark.java
@@ -0,0 +1,12 @@
+//Author : Najmi Hakim
+public class Shark extends Fish {
+	private int sharpTeeth = 50;
+
+	public void Eat() {
+		System.out.println("Eat with " + sharpTeeth + " number of teeth");
+	}
+
+	public void SetSharpTeeth(int teeth) {
+		sharpTeeth = teeth;
+	}
+}
